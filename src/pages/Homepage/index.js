2a872613b62511062530fc4c6f754f09d9c46bb3
Katarina Sjölund <katarina.sjolund@outlook.com>
2018-02-06 12:03:58 +0100

    hero-image url insted

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -7,7 +7,7 @@ class Homepage extends React.Component {
     return (
       <div className="body-container">
         <div className="hero-image-container">
-          <img className="hero-image" src="./christopher-burns-435998.jpg" alt="" />
+          <img className="hero-image" src="https://static1.squarespace.com/static/5a1b5169d74cff5c36e9068c/t/5a360e11e2c48362969a5f35/1513573657743/christopher-burns-435998.jpg" alt="" />
         </div>
         <div className="hero-products">Our Products</div>
         <ProductCollection />
