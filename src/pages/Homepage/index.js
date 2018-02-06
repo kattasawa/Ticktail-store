b0d0576429f88d52d043161c4d5d67d0ead16ad9
Katarina Sjölund <katarina.sjolund@outlook.com>
2018-02-06 11:58:00 +0100

    added hero-image homepage

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -7,7 +7,7 @@ class Homepage extends React.Component {
     return (
       <div className="body-container">
         <div className="hero-image-container">
-          <img className="hero-image" src="/christopher-burns-435998.jpg" alt="" />
+          <img className="hero-image" src="./christopher-burns-435998.jpg" alt="" />
         </div>
         <div className="hero-products">Our Products</div>
         <ProductCollection />
