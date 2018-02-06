59137652b176c13e32318b86ec88e03cb6191723
Katarina Sjölund <katarina.sjolund@outlook.com>
2018-02-06 10:43:56 +0100

    deleted className=(styles.link)

--- code/src/components/footer/index.js
+++ code/src/components/footer/index.js
@@ -9,7 +9,7 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="footer">
-        // className=(styles.link)
+      
         <a className="termsOfService" href="/terms-of-service"><span>Terms of Service</span></a>
         <a className="privacyPolicy" href="/privacy-policy">Privacy Policy</a>
         <button className="helpCenter">Helpcenter</button>
